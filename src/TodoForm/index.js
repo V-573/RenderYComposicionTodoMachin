aef72e47c09cb631023e6590e1df236018bd36b1
vaj573 <jhonedw573@gmail.com>
2024-07-30 21:58:07 -0500

    trabajo realizado en curso introductorio react tema context, portal y desde aca inicia el paso al siguiente curso de render y composicion

---
+++ src/TodoForm/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './TodoForm.css';
+import { TodoContext } from '../TodoContext';
+
+function TodoForm() {
+    const { 
+        addTodo,
+        setOpenModal,
+        
+
+    } = React.useContext(TodoContext);
+
+    const [newTodoValue, setNewTodoValue] = React.useState('');
+
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        addTodo(newTodoValue);
+        setOpenModal(false);
+    };
+
+    const onCancel = () => {
+       
+        setOpenModal(false);
+    };
+
+    const onChange = (e) => {
+        setNewTodoValue(e.target.value);
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label>Escribe tu nuevo TODO</label>
+            <textarea
+                placeholder="cortar cebolla para el almuerzo"
+                value={newTodoValue}
+                onChange={onChange}
+            
+            />
+            <div className='TodoForm-buttonContainer'>
+                <button 
+                    onClick={onCancel}
+                    type='button'
+                className="TodoForm-button TodoForm-button--cancel">
+                Cancelar
+                </button>
+                <button 
+                     type='submit'
+                    className="TodoForm-button TodoForm-button-add"
+                >Añadir</button>
+            </div>
+        </form>
+    );
+
+}
+export { TodoForm };
